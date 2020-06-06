98442ebfeab8ee74988cb0048980d3b230bf40f8
iitenkida7 <i.takeshi2222@gmail.com>
2020-06-07 00:10:48 +0900

    vueAnalytics使ってみる

--- project/src/main.js
+++ project/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from '@/App.vue'
 import router from '@/router.js'
+import VueAnalytics from 'vue-analytics';
 
 Vue.config.productionTip = false
 
@@ -8,3 +9,8 @@ new Vue({
   render: h => h(App),
   router,
 }).$mount('#app')
+
+Vue.use(VueAnalytics, {
+  id: 'UA-43152705-1',
+  router
+});
