02ec0ce53367f414959939732c0a2caae4c08791
iitenkida7 <i.takeshi2222@gmail.com>
2020-06-06 22:44:59 +0900

    IRのコンテンツ移植

--- project/src/components/Ir.vue
+++ project/src/components/Ir.vue
@@ -2,19 +2,39 @@
     <div class="container">
       <div class="row">
         <div class="col-md-1 text-center">
-          <h1 style="font-size:18px">(有)三郷交通</h1><img style="max-width: 260px; height: auto; margin:auto;display: block" src="images/emblem_small.gif"><a class="btn pull-center btn-large btn-primary" style="margin: 20px;" href="tel:048-952-3310">048-952-
-            <ruby>
-              <rb>3310</rb>
-              <rt>みさと</rt>
-            </ruby></a>
-          <p style="font-weight: bold">タクシー呼び出し可能エリア:埼玉県三郷市</p>
-          <hr>
-          <div class="infomation" style="height: 100px; vertical-align: text-top; text-align: left">   
-            <h3 style="font-size: 14px ;background-color: MistyRose">お知らせ</h3>
-            <li><a href="./recruit.html">タクシードライバー大募集</a></li>
+          <h1 style="font-size:24px">会社概要</h1>
+          <table class="table table-striped" id="sortTableExample">
+            <tr>
+              <td stype="width:60px">会社名</td>
+              <td>有限会社三郷交通</td>
+            </tr>
+            <tr>
+              <td>所在地</td>
+              <td>〒341-0034<br>埼玉県三郷市新和３－１４７</td>
+            </tr>
+            <tr>
+              <td>TEL</td>
+              <td> <a href="tel:048-952-3310">048-952-3310</a></td>
+            </tr>
+            <tr>
+              <td>URL</td>
+              <td>http://misato-taxi.jp</td>
+            </tr>
+            <tr>
+              <td>事業内容</td>
+              <td>一般乗用旅客自動車運送<br>(タクシー業)</td>
+            </tr>
+            <tr>
+              <td>主要営業地域</td>
+              <td>埼玉県三郷市</td>
+            </tr>
+          </table>
+          <h1 style="font-size:24px">地図</h1>
+          <div class="text-center">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12941.326056823415!2d139.88213668220905!3d35.816341174994726!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60189ac5bdaa1009%3A0x1c85dff09b156c64!2z44CSMzQxLTAwMzQg5Z-8546J55yM5LiJ6YO35biC5paw5ZKM77yT5LiB55uu77yR77yU77yXIO-8iOacie-8ieS4iemDt-S6pOmAmuacrOekvu-8iOWWtu-8iQ!5e0!3m2!1sja!2sjp!4v1411789149810" width="280" height="250" frameborder="0" style="border:0"></iframe>
           </div>
         </div>
-    </div>
+      </div>
     </div>
 </template>
 <!-- style中身は省略 -->
