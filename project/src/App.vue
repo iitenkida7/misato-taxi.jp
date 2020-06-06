dda8122f7625f5bcdb43f03ec09d93d5c948795a
iitenkida7 <i.takeshi2222@gmail.com>
2020-06-06 22:15:43 +0900

    不要な行削除

--- project/src/App.vue
+++ project/src/App.vue
@@ -8,24 +8,15 @@
 <script>
 import HeaderItem from './components/HeaderItem.vue'
 import TopPage from './components/TopPage.vue'
-//import HelloWorld from './components/HelloWorld.vue'
-
-//import Contents2 from './components/Contents2.vue'
 
 export default {
   name: 'App',
   components: {
-//    HelloWorld,
     HeaderItem,
     TopPage,
-  //  Contents2
   },
-  data() {
-    return { 
-     currentView: 'TopPage' 
   }
-  }
-}
+
 </script>
 
 <style>
