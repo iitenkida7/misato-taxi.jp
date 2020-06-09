5a124fb938afc5a58fcaf3cbefe5857fc6e1eb03
iitenkida7 <i.takeshi2222@gmail.com>
2020-06-10 00:42:00 +0900

    bootstarp4に置き換え。細かいところがダメダメだけど時間無いので、とりあえずある程度まともに見える状態

--- project/src/components/Recruit.vue
+++ project/src/components/Recruit.vue
@@ -1,46 +1,87 @@
 <template>
-    <div class="container">
-      <div class="row">
-        <h2>求人</h2>
-        <hr>
-        <h3 style="font-size:23px ; background-color:#81DAF5">タクシードライバー大募集</h3>
-        <p style="font-size:18px">全車カーナビ付（経験不問）</p><img src="@/assets/img/taxi.jpg">
-        <h4>【仕事の概要】</h4>
+  <main role="main" class="container">
+    <div class="row d-block">
+      <h2>求人</h2>
+      <hr />
+      <h3>タクシードライバー大募集</h3>
+      <p>全車カーナビ付（経験不問）</p>
+      <img src="@/assets/img/taxi.jpg" />
+      <div class="text-center" style="height:30px"></div>
+
+      <h4>【仕事の概要】</h4>
+      <ul>
         <li>地域密着のタクシー会社でのドライバー</li>
-        <h4>【月給】</h4>
+      </ul>
+
+      <h4>【月給】</h4>
+      <ul>
         <li>２５万円〜 月収３５万円以上可</li>
         <li>歩合制なので男・女に関係なく頑張り次第</li>
-        <h4>【勤務時間】</h4>
+      </ul>
+      <h4>【勤務時間】</h4>
+      <ul>
         <li>７：００〜翌日２：００</li>
         <li>月１２日間勤務（月８日も可）</li>
         <li>休憩時間３時間(自宅での休憩も可)</li>
-        <h4>【休日・休暇】</h4>
+      </ul>
+      <h4>【休日・休暇】</h4>
+      <ul>
         <li>会社指定日、有給休暇有り</li>
-        <h4>【対象となる方】</h4>
-        <li><span style="color:red ;font-weight: bold;">女性乗務員大歓迎</span>、定年後の方も大歓迎</li>
+      </ul>
+      <h4>【対象となる方】</h4>
+      <ul>
+        <li>
+          <span style="color:red ;font-weight: bold;">女性乗務員大歓迎</span>、定年後の方も大歓迎
+        </li>
         <li>経験・学歴一切不問</li>
-        <h4>【待遇】</h4>
-        <li>２種免許取得費用、当社全額負担<br>（取得までの間、1日１万円手当支給）</li>
+      </ul>
+      <h4>【待遇】</h4>
+      <ul>
+        <li>
+          ２種免許取得費用、当社全額負担
+          <br />（取得までの間、1日１万円手当支給）
+        </li>
         <li>社会保険完備</li>
         <li>制服貸与</li>
         <li>車通勤OK</li>
         <li>全車カーナビ付きで地理が苦手でもOK</li>
-        <h4>【勤務地】</h4>
+      </ul>
+      <h4>【勤務地】</h4>
+      <ul>
         <li>埼玉県三郷市</li>
-        <hr>
-        <div class="text-center" style="height:200px">
-          <p style="color:red ; font-size:16px ;font-weight: bold;">やる気があればOK<br>親切丁寧にサポートします！まずはお電話を！</p><a class="btn pull-center btn-large btn-primary" href="tel:048-952-3310">048-952-
-            <ruby>
-              <rb>3310</rb>
-              <rt>みさと</rt>
-            </ruby></a>
-        </div>
+      </ul>
+      <hr />
+      <div class="text-center" style="height:30px"></div>
+      <div class="col-sm mx-auto rounded mx-auto d-block">
+        <p class="text-center" style="color:red ; font-size:16px ;font-weight: bold;">
+          やる気があればOK
+          <br />親切丁寧にサポートします！まずはお電話を！
+        </p>
+        <a
+          class="rounded mx-auto d-block btn btn-primary btn-lg"
+          v-on:click="gaEventTel"
+          href="tel:048-952-3310"
+        >
+          048-952-
+          <ruby>
+            <rb>3310</rb>
+            <rt>みさと</rt>
+          </ruby>
+        </a>
       </div>
     </div>
+  </main>
 </template>
+
+<style scoped>
+h3 {
+font-size:23px ;
+ background-color:#81DAF5
+}
+</style>
 
 <script>
 export default {
-  name: 'Recruit'
-}
+  name: "Recruit"
+};
 </script>
--- project/src/components/TopPage.vue
+++ project/src/components/TopPage.vue
@@ -1,33 +1,64 @@
 <template>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-1 text-center">
-          <h1 style="font-size:18px">(有)三郷交通</h1>
-          <img style="max-width: 260px; height: auto; margin:auto;display: block" src="@/assets/img/emblem.png">
-          <a class="btn pull-center btn-large btn-primary" style="margin: 20px;" v-on:click="gaEventTel" href="tel:048-952-3310">048-952-
-            <ruby>
-              <rb>3310</rb>
-              <rt>みさと</rt>
-            </ruby>
-          </a>
-          <p style="font-weight: bold">タクシー呼び出し可能エリア:埼玉県三郷市</p>
-          <hr>
-          <div class="infomation" style="height: 100px; vertical-align: text-top; text-align: left">   
-            <h3 style="font-size: 14px ;background-color: MistyRose">お知らせ</h3>
-            <li> <router-link class="brand" to="recruit">タクシードライバー大募集</router-link></li>
+  <main role="main" class="container">
+    <div class="row justify-content-sm-center">
+      <div class="col-sm mx-auto rounded mx-auto d-block">
+        <img class="rounded mx-auto d-block" src="@/assets/img/emblem.png" />
+        <a
+          class="rounded mx-auto d-block btn btn-primary btn-lg"
+          v-on:click="gaEventTel"
+          href="tel:048-952-3310"
+        >
+          048-952-
+          <ruby>
+            <rb>3310</rb>
+            <rt>みさと</rt>
+          </ruby>
+        </a>
+        <div class="col-sm">
+          <p>タクシー呼び出し可能エリア:埼玉県三郷市</p>
+          <hr />
+          <div class="infomation">
+            <h3>お知らせ</h3>
+            <li>
+              <router-link class="brand" to="recruit">タクシードライバー大募集</router-link>
+            </li>
           </div>
         </div>
+      </div>
     </div>
-    </div>
+  </main>
+  <!-- /.container -->
 </template>
+
+<style scoped>
+.btn {
+  margin: 20px;
+  max-width: 260px;
+}
+
+p {
+  font-weight: bold;
+  text-align: center;
+}
+.infomation {
+  font-size: 14px;
+  vertical-align: text-top;
+  text-align: left;
+}
+
+h3 {
+  font-size: 20px;
+  background-color: MistyRose;
+}
+</style>
 
 <script>
 export default {
-  name: 'TopPage',
-    methods: {
-      gaEventTel() {
-        this.$ga.event('tel', 'misato-taxi', 'call', 1)
-      }
+  name: "TopPage",
+  methods: {
+    gaEventTel() {
+      this.$ga.event("tel", "misato-taxi", "call", 1);
     }
-}
+  }
+};
 </script>
